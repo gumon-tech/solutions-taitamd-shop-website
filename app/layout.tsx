4e7d9883ed3ac4504d6d37c3f9f2c854ac2b68b3
komphet.me <komphet.me@gmail.com>
2026-02-15 00:31:44 +0700

    Update background and comfirm

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "#1B6F6B"
+  themeColor: "#6E8D52"
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
